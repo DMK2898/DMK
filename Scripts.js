433a1b8c56709146ceffb027c6d611ff1b99fe69
Dadullah <mansoorzubairkhan@gmail.com>
2024-09-14 07:37:23 +0500

    Delete Scripts.js

--- Scripts.js
+++
@@ -1,5 +0,0 @@
-// Parallax scrolling effect for the hero section
-window.addEventListener('scroll', function() {
-    const hero = document.querySelector('.hero');
-    hero.style.backgroundPositionY = `${window.scrollY * 0.5}px`;
-});
